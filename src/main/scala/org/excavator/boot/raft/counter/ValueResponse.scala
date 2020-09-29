59b128351dd3a4faba30e67bfc6ef19abb3e491d
CMonkey <42.codemonkey@gmail.com>
2020-09-29 14:17:09 +0800

    change inmutable to ValueResponse

--- src/main/scala/org/excavator/boot/raft/counter/ValueResponse.scala
+++ src/main/scala/org/excavator/boot/raft/counter/ValueResponse.scala
@@ -2,10 +2,12 @@ package org.excavator.boot.raft.counter
 
 import scala.beans.BeanProperty
 
-case class ValueResponse(@BeanProperty value:Long, //成功情况下返回的最新value
+case class ValueResponse(@BeanProperty
+                         var value:Long, //成功情况下返回的最新value
                          @BeanProperty
-                         success:Boolean, // 是否成功
+                         var success:Boolean, // 是否成功
                          @BeanProperty
                          redirect:String, // 发生了重新选举，需要跳转的新leader节点
                          @BeanProperty // 失败情况下的错误信息
-                         errorMsg:String) extends Serializable
+                         errorMsg:String // 失败情况下的错误信息
+                        ) extends Serializable
