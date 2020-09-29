99d6098ef88a314bd12f0dc24d4bcb27b0ba7ad3
CMonkey <42.codemonkey@gmail.com>
2020-09-29 11:46:29 +0800

    add getRequest setRequest getResposne functions

--- src/main/scala/org/excavator/boot/raft/counter/IncrementAndAddClosure.scala
+++ src/main/scala/org/excavator/boot/raft/counter/IncrementAndAddClosure.scala
@@ -4,10 +4,18 @@ import com.alipay.sofa.jraft.{Closure, Status}
 
 // 用于Leader 服务端的接收
 class IncrementAndAddClosure(val counterServer:CounterServer,
-                             val request:IncrementAndGetRequest,
+                             var request:IncrementAndGetRequest,
                              val response:ValueResponse,
                              val done:Closure /* 网络应答callback*/) extends Closure{
   override def run(status: Status): Unit = {
-
+    if(done != null){
+      done.run(status)
+    }
   }
+
+  def getRequest() = request
+
+  def setRequest(request: IncrementAndGetRequest) = this.request = request
+
+  def getResponse() = response
 }
