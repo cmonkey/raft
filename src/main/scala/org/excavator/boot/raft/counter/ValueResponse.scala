b9c0982d33dc03c8cc16f2d8ba8aa93e5a9f729f
CMonkey <42.codemonkey@gmail.com>
2020-09-29 11:39:54 +0800

    add ValueResponse

---
+++ src/main/scala/org/excavator/boot/raft/counter/ValueResponse.scala
@@ -0,0 +1,11 @@
+package org.excavator.boot.raft.counter
+
+import scala.beans.BeanProperty
+
+case class ValueResponse(@BeanProperty value:Long, //成功情况下返回的最新value
+                         @BeanProperty
+                         success:Boolean, // 是否成功
+                         @BeanProperty
+                         redirect:String, // 发生了重新选举，需要跳转的新leader节点
+                         @BeanProperty // 失败情况下的错误信息
+                         errorMsg:String) extends Serializable
