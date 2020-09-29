38a2aeab9dd10f8e926bfe9b5483e52fe33e7438
CMonkey <42.codemonkey@gmail.com>
2020-09-29 15:18:03 +0800

    dd apply to CounterServer

--- src/main/scala/org/excavator/boot/raft/counter/CounterServer.scala
+++ src/main/scala/org/excavator/boot/raft/counter/CounterServer.scala
@@ -1,9 +1,71 @@
 package org.excavator.boot.raft.counter
 
-import com.alipay.sofa.jraft.RaftGroupService
+import java.io.File
+
+import com.alipay.remoting.rpc
+import com.alipay.sofa.jraft.core.TimerManager
+import com.alipay.sofa.jraft.entity.PeerId
+import com.alipay.sofa.jraft.option.NodeOptions
+import com.alipay.sofa.jraft.rpc.impl.BoltRpcServer
+import com.alipay.sofa.jraft.rpc.{RaftRpcServerFactory, RpcServer}
+import com.alipay.sofa.jraft.{Node, RaftGroupService}
+import org.apache.commons.io.FileUtils
 
 class CounterServer {
 
+  // raft 服务端框架
   private var raftGroupService:RaftGroupService  = null
+  // raft 节点
+  private var node:Node = null
+  // 业务状态机
+  private var fsm:CounterStateMachine = null
+
+  def apply(dataPath:String, groupId:String, serverId:PeerId, nodeOptions: NodeOptions) = {
+
+    // init data path
+    FileUtils.forceMkdir(new File(dataPath))
+
+    // init global time
+    val timerManager = new TimerManager(50)
+
+    val remoteRpc = new rpc.RpcServer(serverId.getPort)
+    val rpcServer = new BoltRpcServer(remoteRpc)
+    RaftRpcServerFactory.addRaftRequestProcessors(rpcServer)
+
+    rpcServer.registerProcessor(new GetValueRequestProcessor(this))
+    rpcServer.registerProcessor(new IncrementAndGetRequestProcessor(this))
+
+    this.fsm = new CounterStateMachine
+
+    nodeOptions.setFsm(this.fsm)
+
+    nodeOptions.setLogUri(dataPath + File.separator + "log")
+
+    nodeOptions.setRaftMetaUri(dataPath + File.separator + "raft_meta")
+
+    nodeOptions.setSnapshotUri(dataPath + File.separator + "snapshot")
+
+    this.raftGroupService = new RaftGroupService(groupId,serverId, nodeOptions, rpcServer)
+
+    this.node = this.raftGroupService.start()
+  }
+
+  def getFsm = this.fsm
+
+  def getNode = this.node
+
+  def getRaftGroupService() = this.raftGroupService
+
+  def redirect() = {
+    val response = new ValueResponse(0, false, "", "")
+    if(null != node){
+      val leader = node.getLeaderId
+      if(null != leader){
+        response.redirect = leader.toString
+      }
+    }
+
+    response
+  }
 
 }
